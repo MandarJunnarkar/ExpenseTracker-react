8a4f1814555cc4b78acad236ddea3d17807acef2
Mandar Junnarkar <mandarjunnarkar222@gmail.com>
2022-02-02 23:25:43 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+
+
+  const addExpenseHandler = (expense) =>{
+    setExpenses(prevExpenses =>{
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses expenses={expenses}/>
+    </div>
+  );
+}
+
+export default App;
